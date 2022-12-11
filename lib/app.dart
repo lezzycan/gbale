24bdb434004be338a4a0cd3fbf93e5a57db9c186
Developer Lezzycan <waheedolalekan23@gmail.com>
2022-12-11 02:26:38 +0100

    added config file for the app

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,12 @@
 import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 @StackedApp(
   routes: [],
- 
+  dependencies: [
+    LazySingleton(classType: NavigationService),  
+  ],
+  logger: StackedLogger(),
 )
 class AppSetup {
   /// This serves no purpose rather than using the [StackedApp] notation.
